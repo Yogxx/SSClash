1538ade86d60d141dbe3e8b48851a060c0faddce
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-05-31 23:50:15 +0700

    Update log.js

--- rootfs/www/luci-static/resources/view/ssclash/log.js
+++ rootfs/www/luci-static/resources/view/ssclash/log.js
@@ -50,7 +50,7 @@ function startPolling() {
                             }, (lastLogLength > 0 ? '\n' : '') + processedNewLines);
 
                             lastLogLength = lines.length;
-                            editor.scrollToLine(editor.session.getLength(), false, true, function() {});
+                            editor.scrollToLine(editor.session.getLength(), false, true, function () { });
                         }
                     } else if (lastLogLength > 0) {
                         editor.setValue('', 1);
@@ -72,6 +72,16 @@ function processLogLine(line) {
     return line;
 }
 
+function setLogHeight(height) {
+    const logDiv = document.getElementById('logfile');
+    if (logDiv) {
+        logDiv.style.height = height;
+        setTimeout(() => {
+            if (editor) editor.resize();
+        }, 50); // Delay to ensure DOM updates before resize
+    }
+}
+
 return view.extend({
     load: function () {
         return fs.stat('/sbin/logread').then(stat => {
@@ -80,18 +90,37 @@ return view.extend({
     },
 
     render: function (stat) {
-        const view = E(
-            'div',
-            { class: 'cbi-map' },
+        const extendBtn = E('button', {
+            class: 'btn cbi-button',
+            click: () => setLogHeight('550px')
+        }, 'Extend');
+
+        const shrinkBtn = E('button', {
+            class: 'btn cbi-button',
+            style: 'margin-left: 10px',
+            click: () => setLogHeight('1px')
+        }, 'Shrink');
+
+        const controlRow = E('div', { style: 'margin-bottom: 10px;' }, [
+            extendBtn,
+            shrinkBtn
+        ]);
+
+        const logContainer = E('div', {
+            id: 'logfile',
+            style: 'width: 100% !important; height: 1px; border: 1px solid #ccc;'
+        });
+
+        const view = E('div', { class: 'cbi-map' },
             E('div', { class: 'cbi-section' }, [
-                E('div', {
-                    id: 'logfile',
-                    style: 'width: 100% !important; height: 640px;'
-                })
+                controlRow,
+                logContainer
             ])
         );
 
-        initializeAceEditor();
+        setTimeout(() => {
+            initializeAceEditor();
+        }, 0); // Init after DOM is painted
 
         return view;
     },
